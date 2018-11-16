059a12630d91a73e52fae8c9fbcbb3a147c9624a
oddman <oddmanalalit@gmail.com>
2018-11-16 19:09:40 +0530

    changes for update composer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,13 +83,13 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
+                    <a href="https://laravel.com/docs">Documentation</a>
             </div>
         </div>
     </body>
